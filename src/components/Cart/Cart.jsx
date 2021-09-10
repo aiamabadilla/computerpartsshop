7bd968a3ace588e77c0e875362303ee5e44fbe4e
anabadil <abadillaaiam@gmail.com>
2021-09-09 20:42:07 -0700

    added responsivness to shopping cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -13,24 +13,28 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart, loading })
     return (
         <div className="cart">
             <h2 className="cart__title">Shopping Cart</h2>
-            {loading ? <CircularProgress className="cart__loading" /> : 
-                (cart.line_items.length ? cart.line_items.map((item) => (
-                    <CartItem key={item.id} item={item} 
-                    onRemoveFromCart={onRemoveFromCart}
-                    onEmptyCart={onEmptyCart}
-                    onUpdateCartQty={onUpdateCartQty} />
-         )) : <h5 className="cart__empty">Your cart is empty! Start shopping <Link to="/">here</Link>.</h5>)}
-            <div className="cart__info">
-                <div className="cart__infoHeader">
-                    <div className="cart__items">
-                        <h4 className="cart__itemsAmount">subtotal ({loading ? "0" : cart.line_items.length}) items</h4>
-                        <Button className="cart__deleteAllButton" onClick={() => onEmptyCart()}>
-                            <DeleteForever className="cart__deleteAllIcon" />
-                        </Button>
+            <div className="cart__content">
+                <div className="cart__itemsContainer">
+                    {loading ? <CircularProgress className="cart__loading" /> : 
+                        (cart.line_items.length ? cart.line_items.map((item) => (
+                            <CartItem key={item.id} item={item} 
+                            onRemoveFromCart={onRemoveFromCart}
+                            onEmptyCart={onEmptyCart}
+                            onUpdateCartQty={onUpdateCartQty} />
+                )) : <h5 className="cart__empty">Your cart is empty! Start shopping <Link to="/">here</Link>.</h5>)}
+            </div>
+                <div className="cart__info">
+                    <div className="cart__infoHeader">
+                        <div className="cart__items">
+                            <h4 className="cart__itemsAmount">subtotal ({loading ? "0" : cart.line_items.length}) items</h4>
+                            <Button className="cart__deleteAllButton" onClick={() => onEmptyCart()}>
+                                <DeleteForever className="cart__deleteAllIcon" />
+                            </Button>
+                        </div>
+                        <h5 className="cart__cost">{loading ? "$0" :cart.subtotal.formatted_with_symbol}</h5>
                     </div>
-                    <h5 className="cart__cost">{loading ? "$0" :cart.subtotal.formatted_with_symbol}</h5>
+                    <Button className="cart__button">proceed to checkout</Button>
                 </div>
-                <Button className="cart__button">proceed to checkout</Button>
             </div>
         </div>
     )
